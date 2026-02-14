dd2a06af8134bcaa7a38ad536553251ecc83de31
Prashant <prasantsingh945@gmail.com>
2026-02-15 03:30:32 +0530

    implement dark mode and light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { Provider } from "@/components/ui/provider"
+import { ColorModeProvider } from "./components/ui/color-mode"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
-    <App />
-    </Provider>
+    <ColorModeProvider defaultTheme="light">
+      <Provider >
+        <App />
+      </Provider>    
+    </ColorModeProvider>
   </React.StrictMode>,
 )
