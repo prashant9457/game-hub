163f6593cd4bc1f5c0c22858261c2600efa71fd9
Prashant <prasantsingh945@gmail.com>
2026-09-03 19:49:40 +0530

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,33 @@
+import { Button, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setexpanded] = useState(false);
+  const limit = 300;
+
+  if(!children) return null;
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + "...";
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        size="xs"
+        marginLeft={2}
+        fontWeight="bold"
+        colorPalette="cyan"
+        onClick={() => setexpanded(!expanded)}
+      >
+        {expanded ? "Show Less" : "Read More"}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,3 +1,4 @@
+import ExpandableText from "@/components/ExpandableText";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
@@ -13,7 +14,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading> {game.name} </Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
