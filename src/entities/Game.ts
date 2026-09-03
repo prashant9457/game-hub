29082413e889bcc01a69dae78e8d38e580d8bebc
Prashant <prasantsingh945@gmail.com>
2026-09-03 20:08:25 +0530

    Build Game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genres } from "./Genres";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
+  genres: Genres[];
+  publishers: Publisher[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
